33dcb2362c3f32366fbcb23756022861eb1d6d1a
Cristian Palacios <113551947+cpalacios3@users.noreply.github.com>
2022-09-16 20:06:38 +0000

    uppercase, join lists, reverse all characters extract first word

--- main.py
+++ main.py
@@ -34,57 +34,61 @@
 # You must not modify the already supplied line of code, but must use the appropriate list method to add a new element.
 
 # here are two lists:
-luckyNumbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 11, 12, 13, 14, 15]
-friends = ["Kevin", "Karen", "jim", "oscar", "tim", "lord tennyson"]
-# instructions:
-######join both lists together in a new list
-luckyNumbers.extend(friends)
-###### print the new list out
-print(luckyNumbers)
-###### print out the first item in the list
-print(luckyNumbers[0])
-###### remove the last item of the new list
-newList = luckyNumbers.pop()
-###### print the new list out without the last item
-print(newList)
-###### add three more items to the end of the list
-luckyNumbers.append("florida")
-luckyNumbers.append("illinois")
-luckyNumbers.append("new york")
-###### print the last item in the new list
-print(luckyNumbers)
-###### sort the list
-friends.sort # ignore (.sort does not work)
-print(friends)
-###### find a way to insert a new item at the 3rd position of the list
-luckyNumbers.insert(2, "2.5")
-###### print the new list out
-print(luckyNumbers)
-##### reverse the list
-luckyNumbers.reverse()
-###### print the new list out
-print(luckyNumbers)
-###### print out the length of the new list
-print(len(luckyNumbers))
+# luckyNumbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 11, 12, 13, 14, 15]
+# friends = ["Kevin", "Karen", "jim", "oscar", "tim", "lord tennyson"]
+# # instructions:
+# ######join both lists together in a new list
+# luckyNumbers.extend(friends)
+# ###### print the new list out
+# print(luckyNumbers)
+# ###### print out the first item in the list
+# print(luckyNumbers[0])
+# ###### remove the last item of the new list
+# newList = luckyNumbers.pop()
+# ###### print the new list out without the last item
+# print(newList)
+# ###### add three more items to the end of the list
+# luckyNumbers.append("florida")
+# luckyNumbers.append("illinois")
+# luckyNumbers.append("new york")
+# ###### print the last item in the new list
+# print(luckyNumbers)
+# ###### sort the list
+# friends.sort # ignore (.sort does not work)
+# print(friends)
+# ###### find a way to insert a new item at the 3rd position of the list
+# luckyNumbers.insert(2, "2.5")
+# ###### print the new list out
+# print(luckyNumbers)
+# ##### reverse the list
+# luckyNumbers.reverse()
+# ###### print the new list out
+# print(luckyNumbers)
+# ###### print out the length of the new list
+# print(len(luckyNumbers))
 #####################################challenge 2#######################################################
 # Join the following list into a string, separating each item with a space. Use the appropriate list/string method, and display the result.
 word_list = ["Simple", "is", "better", "than", "complex."]
+print (' '.join(word_list))
 
 # Print the following text in uppercase, using the specific string method:
-
 text1 = "Especially in electronic communications, writing in all caps is equivalent to yelling."
+print(text1.upper())
 
 #substrings
 # Take every third character starting from the ninth to the end of the sentence, and print the result.
-
 text2 = "Never trust a computer you can't throw out a window"
+print(text2[8::3])
 
 # Reverses the position of all the characters in the following sentence and displays the result on the screen.
-
 text3 = "It's great to work with computers. They don't argue, they remember everything and they don't drink your beer"
+print(text3[::-1])
 
 # Extract the first word of the following sentence using slicing, and display it on the screen:
-"Controlling complexity is the essence of programming"
+sentence = "Controlling complexity is the essence of programming"
+all_words = sentence.split()
+print("The first word is " + all_words[0])
+
 
 # create a text input that asks for choice of food
 #create an empty list variable called food
